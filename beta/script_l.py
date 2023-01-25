c1e4e4f83e33798d190517659ee6fab37a50ea5a
perdub <81479253+lesh6295-png@users.noreply.github.com>
2023-01-25 15:31:02 +0300

    Create script_l.py

---
+++ beta/script_l.py
@@ -0,0 +1,23 @@
+
+
+def queueRequests(target, wordlists):
+    engine = RequestEngine(endpoint=target.endpoint,
+                           concurrentConnections=50,
+                           requestsPerConnection=1000,
+                           pipeline=False
+                           )
+    comments = open('beta/comments.txt').readlines()
+    end = open('beta/end.txt').readlines()
+    start = open('beta/start.txt').readlines()
+    errors = open('beta/errors.txt').readlines()
+
+    while 1:
+        err = random.choice(errors)
+        engine.queue(target.req, [err.rstrip(),random.choice(start),err,random.choice(end), random.choice(comments), random.randint(0,35000000)])
+        #engine.queue(target.req, [words[word].rstrip(),random.choice(words),random.choice(words),random.choice(words)])
+
+
+def handleResponse(req, interesting):
+    # currently available attributes are req.status, req.wordcount, req.length and req.response
+    if req.status != 200:
+        table.add(req)
